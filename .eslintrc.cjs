c60c1621aea057d83ce80293785c3b5d5a7f7bb5
naftali <48069590+naftali100@users.noreply.github.com>
2023-07-30 12:50:26 +0300

    add template

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    // TODO: 'plugin:@typescript-eslint/recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:vuejs-accessibility/recommended',
+    'prettier'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  // plugins: ['vue', 'vuejs-accessibility', '@typescript-eslint'],
+  rules: {
+    'no-undef': 'off'
+  }
+}
---
+++ tests/test.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from 'vitest'
+
+describe('hello world', () => {
+  test('hello world called', () => {
+      expect(true).toBe(true)
+  })
+})
